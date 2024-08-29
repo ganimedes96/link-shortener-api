e160a3c7eadfd43a0665582b16b950012bfb33c8
ganimedes <ganimedes409@gmail.com>
2024-08-28 22:26:04 -0300

    finish

---
+++ go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.22.6
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.1.0
+	golang.org/x/exp v0.0.0-20240823005443-9b4947da3948
+)
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log/slog"
+	"main/api"
+	"net/http"
+	"time"
+)
+
+func main() {
+	if err := run(); err != nil {
+		slog.Error("failed to execute code ", "error", err)
+		return
+	}
+	slog.Info("all system offline")
+}
+
+func run() error {
+	db := make(map[string]string)
+	handler := api.NewHandler(db)
+	s := http.Server{
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  time.Minute,
+		Addr:         ":8080",
+		Handler:      handler,
+	}
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+	return nil
+}
